62893af4ce57982019f27f087f5f814aaa6fee3f
Audrey Castillo <valax321@gmail.com>
2025-12-07 07:05:37 +1100

    More control for material from texture creation

--- Editor/CreateMaterialsFromSelection.cs
+++ Editor/CreateMaterialsFromSelection.cs
@@ -9,7 +9,7 @@ namespace Radish
 {
     internal static class CreateMaterialFromSelection
     {
-        [MenuItem("Assets/Create/Materials From Selection", false, 302)]
+        [MenuItem("Assets/Create/Materials/From Selection", false, 302)]
         private static void Create()
         {
             AssetDatabase.StartAssetEditing();
@@ -47,6 +47,57 @@ namespace Radish
 
                 var texPath = AssetDatabase.GetAssetPath(asset);
                 var matPath = Path.ChangeExtension(texPath, "mat");
+                matPath = AssetDatabase.GenerateUniqueAssetPath(matPath);
+                AssetDatabase.CreateAsset(mat, matPath);
+                mats.Add(mat);
+            }
+
+            AssetDatabase.StopAssetEditing();
+            Selection.objects = mats.Select(x => (Object)x).ToArray();
+        }
+        
+        [MenuItem("Assets/Create/Materials/From Selection (No Dupes)", false, 303)]
+        private static void CreateNoDup()
+        {
+            AssetDatabase.StartAssetEditing();
+
+            List<Material> mats = new();
+
+            foreach (var asset in Selection.GetFiltered<Texture>(SelectionMode.Assets))
+            {
+                var texPath = AssetDatabase.GetAssetPath(asset);
+                var matPath = Path.ChangeExtension(texPath, "mat");
+
+                if (AssetDatabase.AssetPathExists(matPath))
+                    continue;
+                
+                Material mat;
+#if UNITY_6000_0_OR_NEWER
+                if (GraphicsSettings.defaultRenderPipeline)
+#else
+                if (GraphicsSettings.renderPipelineAsset)
+#endif
+                {
+                    mat = new Material(GraphicsSettings.currentRenderPipeline.defaultMaterial)
+                    {
+                        name = asset.name,
+                        mainTexture = asset,
+                        color = Color.white
+                    };
+                }
+                else
+                {
+                    mat = new Material(Shader.Find("Standard"))
+                    {
+                        name = asset.name,
+                        mainTexture = asset,
+                        color = Color.white
+                    };
+                }
+
+                if (!mat)
+                    return;
+                
                 matPath = AssetDatabase.GenerateUniqueAssetPath(matPath);
                 AssetDatabase.CreateAsset(mat, matPath);
                 mats.Add(mat);
